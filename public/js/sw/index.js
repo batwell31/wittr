b5265d743304c81393c611c7112fb2a603c17590
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-07-13 17:01:31 -0400

    add activate eventListener to delete previous cache versions

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -15,6 +15,20 @@ self.addEventListener('install', function (event) {
     );
 });
 
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('wittr-') && cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {
