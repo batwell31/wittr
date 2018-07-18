34d3fb81fcadc1f32b6ac3bbda4947f37cb718b5
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-07-17 20:48:35 -0400

    update IndexController.js

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -11,18 +11,18 @@ export default function IndexController(container) {
   this._registerServiceWorker();
 }
 
-IndexController.prototype._registerServiceWorker = function() {
+IndexController.prototype._registerServiceWorker = function () {
   if (!navigator.serviceWorker) return;
 
   var indexController = this;
 
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  navigator.serviceWorker.register('/sw.js').then(function (reg) {
     if (!navigator.serviceWorker.controller) {
       return;
     }
 
     if (reg.waiting) {
-      indexController._updateReady();
+      indexController._updateReady(reg.waiting);
       return;
     }
 
@@ -31,30 +31,43 @@ IndexController.prototype._registerServiceWorker = function() {
       return;
     }
 
-    reg.addEventListener('updatefound', function() {
+    reg.addEventListener('updatefound', function () {
       indexController._trackInstalling(reg.installing);
     });
   });
+
+  // Ensure refresh is only called once.
+  // This works around a bug in "force update on reload".
+  var refreshing;
+  navigator.serviceWorker.addEventListener('controllerchange', function () {
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+  });
 };
 
-IndexController.prototype._trackInstalling = function(worker) {
+IndexController.prototype._trackInstalling = function (worker) {
   var indexController = this;
-
-  worker.addEventListener('statechange', function() {
-    if (worker.state === 'installed') {
-      indexController._updateReady();
+  worker.addEventListener('statechange', function () {
+    if (worker.state == 'installed') {
+      indexController._updateReady(worker);
     }
   });
 };
 
-IndexController.prototype._updateReady = function() {
+IndexController.prototype._updateReady = function (worker) {
   var toast = this._toastsView.show("New version available", {
-    buttons: ['whatever']
+    buttons: ['refresh', 'dismiss']
+  });
+
+  toast.answer.then(function (answer) {
+    if (answer != 'refresh') return;
+    worker.postMessage({ action: 'skipWaiting' });
   });
 };
 
 // open a connection to the server for live updates
-IndexController.prototype._openSocket = function() {
+IndexController.prototype._openSocket = function () {
   var indexController = this;
   var latestPostDate = this._postsView.getLatestPostDate();
 
@@ -73,33 +86,33 @@ IndexController.prototype._openSocket = function() {
   var ws = new WebSocket(socketUrl.href);
 
   // add listeners
-  ws.addEventListener('open', function() {
+  ws.addEventListener('open', function () {
     if (indexController._lostConnectionToast) {
       indexController._lostConnectionToast.hide();
     }
   });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
+  ws.addEventListener('message', function (event) {
+    requestAnimationFrame(function () {
       indexController._onSocketMessage(event.data);
     });
   });
 
-  ws.addEventListener('close', function() {
+  ws.addEventListener('close', function () {
     // tell the user
     if (!indexController._lostConnectionToast) {
       indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
     }
 
     // try and reconnect in 5 seconds
-    setTimeout(function() {
+    setTimeout(function () {
       indexController._openSocket();
     }, 5000);
   });
 };
 
 // called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function(data) {
+IndexController.prototype._onSocketMessage = function (data) {
   var messages = JSON.parse(data);
   this._postsView.addPosts(messages);
 };
